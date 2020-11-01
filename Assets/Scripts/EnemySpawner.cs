efe6ad86f150f988f02cbef252e83bf7e812425c
Marion MOUNAIX <mamounaix@airfrance.fr>
2020-11-01 21:57:58 +0100

    Add boot enemy

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class EnemySpawner : NetworkBehaviour
+{
+    // prefab de notre ennemi
+    public GameObject enemyPrefab;
+
+    // nombre d'ennemis à faire apparaitre
+    public int numberOfEnemies;
+
+    public override void OnStartServer()
+    {
+        // Apparition du nombre d'ennemis lors du lancement du serveur
+        for (int i = 0; i < numberOfEnemies; i++)
+        {
+            var spawnPosition = new Vector3(
+                Random.Range(-8.0f, 8.0f),
+                0.0f,
+                Random.Range(-8.0f, 8.0f)
+            );
+
+            var spawnRotation = Quaternion.Euler(
+                0.0f,
+                Random.Range(0, 180),
+                0.0f
+            );
+
+            var enemy = (GameObject)Instantiate(enemyPrefab, spawnPosition, spawnRotation);
+            NetworkServer.Spawn(enemy);
+        }
+    }
+
+}
--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -2,28 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Networking;
 
-public class Health : MonoBehaviour
+public class Health : NetworkBehaviour
 {
     public const int maxHealth = 100;
+
+    [SyncVar(hook = "OnChangeHealth")]
     public int currentHealth = maxHealth;
+    public bool destroyOnDeath;
 
     public RectTransform healthBar;
-
+  
     // Prendre des dégats
     public void TakeDamage(int amount)
     {
+        if (!isServer)
+        {
+            return;
+        }
         // On baisse la vie actuelle selon le nombre de dégats
         currentHealth -= amount;
 
         // Si la vie actuelle atteint 0 le personnage meurt
         if (currentHealth <= 0)
         {
-            currentHealth = 0;
-            Debug.Log("Dead !!!!!");
+            if (destroyOnDeath)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                currentHealth = maxHealth;
+                RpcRespawn();
+            }
+            
         }
-
-        healthBar.sizeDelta = new Vector2(currentHealth,healthBar.sizeDelta.y);
         
     }    
+
+    void OnChangeHealth(int health)
+    {
+        healthBar.sizeDelta = new Vector2(health, healthBar.sizeDelta.y);
+    }
+
+    [ClientRpc]
+    void RpcRespawn()
+    {
+        if (isLocalPlayer)
+        {
+            // on replace le joueur aux positions (0,0,0)
+            transform.position = Vector3.zero;
+        }
+    }
 }

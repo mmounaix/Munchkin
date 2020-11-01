eb8440f1bbbe66c4879386fb5c686c7b458b8234
Marion MOUNAIX <mamounaix@airfrance.fr>
2020-11-01 20:59:47 +0100

    Add health bar

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,7 +4,16 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-  void OnCollisionEnter () {
+  void OnCollisionEnter(Collision collision) 
+  {
+      // Récupération de l'objet touché par la balle
+      var hit = collision.gameObject;
+      var health = hit.GetComponent<Health>();
+      if (health != null)
+      {
+          // Application des dégats
+          health.TakeDamage(10);
+      }
       Destroy(gameObject);
   }
 }
--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,8 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
-   
+    public const int maxHealth = 100;
+    public int currentHealth = maxHealth;
+
+    public RectTransform healthBar;
+
+    // Prendre des dégats
+    public void TakeDamage(int amount)
+    {
+        // On baisse la vie actuelle selon le nombre de dégats
+        currentHealth -= amount;
+
+        // Si la vie actuelle atteint 0 le personnage meurt
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            Debug.Log("Dead !!!!!");
+        }
+
+        healthBar.sizeDelta = new Vector2(currentHealth,healthBar.sizeDelta.y);
+        
+    }    
 }
